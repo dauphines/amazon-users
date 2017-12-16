08fd61a4d1a2af8262653ed2786de3dc805090a9
v1x1e <github@victoriahchang.com>
2017-12-16 11:59:26 -0800

    Add Express and Sequelize as dependencies

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Aloha Universe!'));
-app.listen(7331, () => console.log('http://localhost:7331'));
+app.listen(7331, () => console.log('----- server : http://localhost:7331 -----'));
