c18d45afe1f60fadf55f61ee7adea53809935f04
v1x1e <github@victoriahchang.com>
2017-12-18 18:00:06 -0800

    Fix getTrialEndDate

--- data_generator/generateMemberships.js
+++ data_generator/generateMemberships.js
@@ -9,35 +9,29 @@ const calendar = {
   1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31
 };
 
-const getEndMonth = (totalDays, startMonth) => {
-  if (totalDays > calendar[startMonth]) {
-    if (startMonth === 12) {
-      return 1;
-    } else {
-      return startMonth + 1;
-    }
-  } else {
-    return startMonth;
-  }
-};
-
-const getEndDate = (totalDays, startMonth) => {
-  if (totalDays === calendar[startMonth]) {
-    return totalDays;
-  } else {
-    return totalDays - calendar[startMonth];
-  }
-};
-
 const getTrialEndDate = (signupDate) => {
   const trialDays = 30;
   let startMonth = signupDate.getMonth() + 1;
-  let startDate = signupDate.getDate();
-  let totalDays = startDate + trialDays;
-  let endMonth = getEndMonth(totalDays, startMonth);
-  let endDate = getEndDate(totalDays, startMonth);
+  let startDay = signupDate.getDate();
+  let totalDays = trialDays + startDay;
+  let endMonth = '';
+  let endDay = '';
 
-  return `${endMonth}/${endDate}/17`;
+  if (totalDays <= calendar[startMonth]) {
+    endMonth = startMonth;
+    endDay = startDay;
+  } else if (startMonth === 2) {
+    endMonth = startMonth + 1;
+    endDay = (trialDays + startDay) - calendar[startMonth];
+  } else if (startMonth === 12) {
+    endMonth = 1;
+    endDay = (trialDays + startDay) - calendar[endMonth];
+  } else {
+    endMonth = startMonth + 1;
+    endDay = (trialDays + startDay) - calendar[endMonth];
+  }
+
+  return `${endMonth}/${endDay}/17`;
 };
 
 const randomizeCancelTrial = (signupDate, endDate) => {
@@ -56,7 +50,7 @@ const removeTimeZone = (date) => {
 console.log('\x1b[36m -------- start ------- \x1b[0m');
 
 stream.once('open', (fd) => {
-  for (let id = 1; id <= 2500000; id++) {
+  for (let id = 1; id <= 250; id++) {
     let prime_status = faker.helpers.randomize(prime_options);
     let total_spend_trial_signup = Math.floor(faker.finance.amount() * 100);
     let trial_signup_date = faker.date.between('01/01/17', '12/31/17');
